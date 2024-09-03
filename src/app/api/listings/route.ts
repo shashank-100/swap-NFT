39249eeb9adcfc1d1d8571ae250f95ce5a0c22e3
Shashank Telkhade <shashank.telkhade@gmail.com>
2024-09-03 17:03:23 +0530

    removed default

--- src/app/api/listings/route.ts
+++ src/app/api/listings/route.ts
@@ -2,10 +2,9 @@ import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from 'next/server';
 
-export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-    const { id } = req.query;
-
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
     try {
+        const { id } = req.query;
         const response = await axios.get(`https://api-mainnet.magiceden.dev/v2/tokens/${id}/listings`, {
             headers: {
                 "Accept": "application/json"
