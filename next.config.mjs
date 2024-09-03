6ff80235befb32f837895cb61d5801393da22110
Shashank Telkhade <shashank.telkhade@gmail.com>
2024-09-03 17:15:08 +0530

    applied cors to all routes

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,10 @@ const nextConfig = {
         return [
           {
             // matching all API routes
-            source: "/src/app/lib/:path*",
+            source: "/src/app/:path*",
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
+              { key: "Access-Control-Allow-Origin", value: "http" },
               { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
               { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
             ]
