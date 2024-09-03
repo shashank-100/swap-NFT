fcf6e94e351f1c272a352e0ea9631343f06da74e
Shashank Telkhade <shashank.telkhade@gmail.com>
2024-09-03 17:09:14 +0530

    NextApiReq -> NextRequest

--- src/app/api/listings/route.ts
+++ src/app/api/listings/route.ts
@@ -2,16 +2,19 @@ import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
     try {
-        const { id } = req.query;
+        const id = req.nextUrl.searchParams.get("id");
+        if (!id) {
+            return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+        }
         const response = await axios.get(`https://api-mainnet.magiceden.dev/v2/tokens/${id}/listings`, {
             headers: {
                 "Accept": "application/json"
             }
         });
 
-        return NextResponse.json(response);
+        return NextResponse.json(response, {status: 200});
     } catch (error: any) {
         return NextResponse.error()
     }
